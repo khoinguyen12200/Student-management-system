af889d99e6a79c6bf7f85dd12610924e04b74e83
khoinguyen12200 <63348801+khoinguyen12200@users.noreply.github.com>
2021-09-02 19:17:06 +0700

    instructor

--- app.py
+++ app.py
@@ -26,18 +26,17 @@ pool = mysql.connector.pooling.MySQLConnectionPool(
     pool_name="mypool",
     pool_size=10,
     autocommit=True,
-    host="localhost", 
-    user='root', 
-    password="", 
+    host="localhost",
+    user='root',
+    password="",
     database='htqlsv')
 
 
-
-def sqlExecute(sql,args=[],fetch = 0):
+def sqlExecute(sql, args=[], fetch=0):
     try:
         connection = pool.get_connection()
         cursor = connection.cursor(dictionary=True)
-        cursor.execute(sql,args)
+        cursor.execute(sql, args)
 
         result = None
         if(fetch == 0):
@@ -55,15 +54,9 @@ def sqlExecute(sql,args=[],fetch = 0):
         return 0
 
 
-
-
-
-
-
 @app.route("/")
 def main():
     return "Welcome!"
-
 
 
 @app.post("/api/manager/login")
@@ -73,7 +66,8 @@ def sign_in():
     account = data['account']
     password = data['password']
 
-    user = sqlExecute("SELECT * FROM manager where ACCOUNT = %s and PASSWORD = %s", [account, password], 1)
+    user = sqlExecute(
+        "SELECT * FROM manager where ACCOUNT = %s and PASSWORD = %s", [account, password], 1)
 
     if(user == None):
         return jsonify({"e": True, "m": "Tài khoản mật khẩu không khớp"})
@@ -92,7 +86,8 @@ def auto_login():
     app.logger.info(account)
     if(account != None):
 
-        info = sqlExecute("SELECT * FROM manager where ACCOUNT = %s;", [account],1)
+        info = sqlExecute(
+            "SELECT * FROM manager where ACCOUNT = %s;", [account], 1)
 
         if(info != None):
             return jsonify({"info": info})
@@ -112,7 +107,6 @@ def changePassword():
     updated = sqlExecute("UPDATE manager set PASSWORD = %s where ACCOUNT = %s and PASSWORD = %s", [
         newPassword, account, oldPassword])
 
-
     if(updated != 0):
         return jsonify({"m": "Đổi mật khẩu thành công"})
     else:
@@ -131,14 +125,13 @@ def changeName():
     updated = sqlExecute("UPDATE manager set FULL_NAME = %s where ACCOUNT = %s and PASSWORD = %s", [
         newName, account, password])
 
-
     if(updated != 0):
         return jsonify({"m": "Đổi tên thành công"})
     else:
         return jsonify({"e": True, "m": "Lỗi xảy ra, không có gì thay đổi"})
 
 
-############# KHOA
+# KHOA
 
 @app.get("/api/manager/department")
 @jwt_required()
@@ -166,7 +159,8 @@ def insertDepartment():
         insert = sqlExecute("INSERT INTO `department`(`id`,`NAME`, `SORT_NAME`) VALUES (%s,%s,%s)", [
                             id, name, sortName])
     else:
-        insert = sqlExecute("INSERT INTO `department`(`NAME`, `SORT_NAME`) VALUES (%s,%s)", [name, sortName])
+        insert = sqlExecute(
+            "INSERT INTO `department`(`NAME`, `SORT_NAME`) VALUES (%s,%s)", [name, sortName])
 
     if(insert != 0):
         return jsonify({"m": "Thêm thành công"})
@@ -188,17 +182,19 @@ def updateDepartment():
     numberChange = 0
 
     if(name):
-        changed = sqlExecute( "UPDATE `department` set NAME = %s where id = %s", [name, id])
+        changed = sqlExecute(
+            "UPDATE `department` set NAME = %s where id = %s", [name, id])
         numberChange += changed
 
     if(sortName):
-        changed = sqlExecute("UPDATE `department` set SORT_NAME = %s where id = %s", [sortName, id])
+        changed = sqlExecute(
+            "UPDATE `department` set SORT_NAME = %s where id = %s", [sortName, id])
         numberChange += changed
 
     if(newId):
-        changed = sqlExecute("UPDATE `department` set ID = %s where id = %s", [newId, id])
+        changed = sqlExecute(
+            "UPDATE `department` set ID = %s where id = %s", [newId, id])
         numberChange += changed
-
 
     if(numberChange != 0):
         return jsonify({"m": "Có "+str(numberChange)+" thay đổi được cập nhật"})
@@ -214,13 +210,13 @@ def deleteDepartment():
     id = body.get("id")
     password = body.get("password")
 
-    user = sqlExecute("select * from manager where ACCOUNT = %s and PASSWORD = %s", [account, password],1)
- 
+    user = sqlExecute(
+        "select * from manager where ACCOUNT = %s and PASSWORD = %s", [account, password], 1)
+
     if(user == None):
         return jsonify({"e": True, "m": "Sai mật khẩu xác nhận"})
 
     deleted = sqlExecute("DELETE from `department` where ID = %s", [id])
-  
 
     if(deleted == 0):
         return jsonify({"m": "Xóa thành công"})
@@ -249,10 +245,10 @@ def insertMajor():
     department = body.get("department")
 
     insert = 0
- 
+
     if(id == None):
-        insert =sqlExecute("INSERT INTO `major`(`NAME`, `SORT_NAME`, `DEPARTMENT`) VALUES (%s,%s,%s)", [
-                            name, sortName, department])
+        insert = sqlExecute("INSERT INTO `major`(`NAME`, `SORT_NAME`, `DEPARTMENT`) VALUES (%s,%s,%s)", [
+            name, sortName, department])
     else:
         insert = sqlExecute("INSERT INTO `major`(`ID`, `NAME`, `SORT_NAME`, `DEPARTMENT`) VALUES (%s,%s,%s,%s)", [
                             id, name, sortName, department])
@@ -275,20 +271,22 @@ def updatetMajor():
 
     updated = 0
 
-
     if(newId != None):
-        update = sqlExecute("update major set ID = %s where ID = %s", [newId, id])
+        update = sqlExecute(
+            "update major set ID = %s where ID = %s", [newId, id])
         updated += update
     if(name != None):
-        update = sqlExecute("update major set NAME = %s where ID = %s", [name, id])
+        update = sqlExecute(
+            "update major set NAME = %s where ID = %s", [name, id])
         updated += update
     if(sortName != None):
-        update = sqlExecute( "update major set SORT_NAME = %s where ID = %s", [sortName, id])
+        update = sqlExecute(
+            "update major set SORT_NAME = %s where ID = %s", [sortName, id])
         updated += update
     if(department != None):
-        update = sqlExecute("update major set DEPARTMENT = %s where ID = %s", [department, id])
+        update = sqlExecute(
+            "update major set DEPARTMENT = %s where ID = %s", [department, id])
         updated += update
-
 
     if(updated != 0):
         return jsonify({"m": 'Đã thay đổi '+str(updated)+' giá trị'})
@@ -303,12 +301,13 @@ def deleteMajor():
     id = body.get("id")
     password = body.get("password")
 
-    user = sqlExecute("SELECT * from manager where ACCOUNT = %s and PASSWORD = %s", [account, password],1)
- 
+    user = sqlExecute(
+        "SELECT * from manager where ACCOUNT = %s and PASSWORD = %s", [account, password], 1)
+
     if(user == None):
         return jsonify({"e": True, "m": "Xác nhận mật khẩu sai"})
 
-    delete = sqlExecute( "DELETE FROM major where ID = %s", [id])
+    delete = sqlExecute("DELETE FROM major where ID = %s", [id])
     if(delete == 0):
         return jsonify({"e": True, "m": "Xóa không thành công"})
     return jsonify({"m": "Xóa thành công"})
@@ -317,7 +316,7 @@ def deleteMajor():
 ########################## LỚP CHUYÊN NGÀNH ##########################
 
 
-@app.get("/api/manager/class-major")
+@app.get("/api/class-major")
 @jwt_required()
 def getClassMajor():
 
@@ -325,7 +324,7 @@ def getClassMajor():
     return jsonify(majors)
 
 
-@app.post("/api/manager/class-major/insert")
+@app.post("/api/class-major/insert")
 @jwt_required()
 def insertClassMajor():
 
@@ -336,53 +335,41 @@ def insertClassMajor():
     instructor = body.get("instructor")
 
     insert = 0
- 
-    if(instructor != None):
-        insert =sqlExecute("INSERT INTO `class`(`NAME`, `MAJOR`, `COURSE`, `INSTRUCTOR`) VALUES (%s,%s,%s,%s)", [
-                            name, major, course])
-    else:
-        insert = sqlExecute("INSERT INTO `class`(`NAME`, `MAJOR`, `COURSE`, ) VALUES (%s,%s,%s)", [
-                            name, major, course, instructor])
+
+    print("instructor")
+    print(str(instructor))
+
+    insert = sqlExecute("INSERT INTO `class`(`NAME`, `MAJOR`, `COURSE`, `INSTRUCTOR`) VALUES (%s,%s,%s,%s)", [
+            name, major, course,instructor])
 
     if(insert != 0):
         return jsonify({"m": "Đã thêm thành công"})
     return jsonify({"e": True, "m": "Có lỗi xảy ra"})
 
 
-@app.post("/api/manager/class-major/update")
+@app.post("/api/class-major/update")
 @jwt_required()
 def updateClassMajor():
 
     body = request.get_json()
-    id= body.get("id")
+    id = body.get("id")
     name = body.get("name")
     major = body.get("major")
     course = body.get("course")
     instructor = body.get("instructor")
 
-    updated = 0
+    
 
-
-    if(major != None):
-        update = sqlExecute("update class set MAJOR = %s where ID = %s", [major, id])
-        updated += update
-    if(name != None):
-        update = sqlExecute("update class set NAME = %s where ID = %s", [name, id])
-        updated += update
-    if(course != None):
-        update = sqlExecute( "update class set COURSE = %s where ID = %s", [course, id])
-        updated += update
-    if(instructor != None):
-        update = sqlExecute("update class set INSTRUCTOR = %s where ID = %s", [instructor, id])
-        updated += update
-
+    updated = sqlExecute(
+        "update class set MAJOR = %s, NAME = %s, COURSE = %s, INSTRUCTOR = %s where ID = %s",
+         [major,name,course,instructor, id])
 
     if(updated != 0):
-        return jsonify({"m": 'Đã thay đổi '+str(updated)+' giá trị'})
+        return jsonify({"m": 'Đã thay đổi thành công'})
     return jsonify({"e": True, "m": "Có lỗi xảy ra"})
 
 
-@app.post("/api/manager/class-major/delete")
+@app.post("/api/class-major/delete")
 @jwt_required()
 def deleteClassMajor():
     account = get_jwt_identity()
@@ -390,15 +377,88 @@ def deleteClassMajor():
     id = body.get("id")
     password = body.get("password")
 
-    user = sqlExecute("SELECT * from manager where ACCOUNT = %s and PASSWORD = %s", [account, password],1)
- 
+    user = sqlExecute(
+        "SELECT * from manager where ACCOUNT = %s and PASSWORD = %s", [account, password], 1)
+
     if(user == None):
         return jsonify({"e": True, "m": "Xác nhận mật khẩu sai"})
 
-    delete = sqlExecute( "DELETE FROM class where ID = %s", [id])
-    if(delete != 0):
+    delete = sqlExecute("DELETE FROM class where ID = %s", [id])
+    if(delete == 0):
         return jsonify({"e": True, "m": "Xóa không thành công"})
     return jsonify({"m": "Xóa thành công"})
+
+
+@app.get("/api/instructor")
+@jwt_required()
+def getInstructor():
+    account = get_jwt_identity()
+    body = request.get_json()
+
+    result = sqlExecute("select * from instructor where 1",[],2)
+    return jsonify(result)
+
+@app.post("/api/instructor")
+@jwt_required()
+def postInstructor():
+    account = get_jwt_identity()
+    body = request.get_json()
+    instructorId = body.get('instructorId')
+    fullName = body.get("fullName")
+    dateOfBirth = body.get("dateOfBirth")
+    gender = body.get("gender")
+    citizenId = body.get("citizenId")
+
+    addResult = sqlExecute("INSERT INTO `instructor`(`INSTRUCTOR_ID`,`FULL_NAME`, `DATE_OF_BIRTH`, `GENDER`, `CITIZEN_ID`) VALUES (%s,%s,%s,%s,%s)"
+    , [instructorId,fullName,dateOfBirth,gender,citizenId])
+    if(addResult):
+        return jsonify({"m":"Thêm thành công"})
+
+    print(addResult)
+    return jsonify({"m":"Có lỗi xảy ra","e": True})
+
+
+@app.put("/api/instructor")
+@jwt_required()
+def putInstructor():
+    account = get_jwt_identity()
+    body = request.get_json()
+    id = body.get("id")
+    instructorId = body.get('instructorId')
+    fullName = body.get("fullName")
+    dateOfBirth = body.get("dateOfBirth")
+    gender = body.get("gender")
+    citizenId = body.get("citizenId")
+
+    updateResult = sqlExecute("UPDATE `instructor` SET `INSTRUCTOR_ID`=%s,`FULL_NAME`=%s,`DATE_OF_BIRTH`=%s,`GENDER`=%s,`CITIZEN_ID`=%s WHERE ID = %s"
+    , [instructorId,fullName,dateOfBirth,gender,citizenId,id])
+    if(updateResult):
+        return jsonify({"m":"Thay đổi đã lưu thành công"})
+
+    print(updateResult)
+    return jsonify({"m":"Có lỗi xảy ra","e": True})
+
+@app.delete("/api/instructor")
+@jwt_required()
+def deleteInstructor():
+    account = get_jwt_identity()
+    body = request.get_json()
+    id = body.get("id")
+    password = body.get("password")
+
+    user = sqlExecute(
+        "SELECT * from manager where ACCOUNT = %s and PASSWORD = %s", [account, password], 1)
+
+    if(user == None):
+        return jsonify({"e": True, "m": "Xác nhận mật khẩu sai"})
+
+    deleteResult = sqlExecute("DELETE FROM instructor where ID = %s",[id])
+    if(deleteResult):
+        return jsonify({"m":"Xóa thành công"})
+
+    print(deleteResult)
+    return jsonify({"m":"Có lỗi xảy ra","e": True})
+
 
 if __name__ == "__main__":
     if prod:
